cd69fdfc4dd806482f36ca18d41ebcaebf9503c0
F4lc0131 <nicolaflavio22@gmail.com>
2020-12-04 13:34:02 +0100

    Add files via upload

---
+++ pytest.py
@@ -0,0 +1,8 @@
+﻿from BotOS_API import *
+def command():
+	try:
+        PyBot.SendTextMessage("Comando di prova installato!")
+	except Exception as ex:
+		PyBot.SendTextMessage("<b>Errore durante l'esecuzione di pyinstall</b>\n<i>" + str(ex) + "</i>")
+
+command()

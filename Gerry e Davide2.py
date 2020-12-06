53bd7edf0fc120676a19004f1fe80de051406c4a
w13rsm0 <56513487+w13rsm0@users.noreply.github.com>
2020-12-06 12:12:19 +0100

    Add files via upload

--- Gerry e Davide2.py
+++ Gerry e Davide2.py
@@ -5,6 +5,6 @@ if args is not None:
     if args[0]=="Davide":
         PyBot.SendTextMessage("MA SIAMO AL MILITARE QUI!!!!!!")
     if args[1]=="Gerry":
-        PyBot.SendTextMessage("<b>MA NOOO MA NON È VERO!!!</b>")
+        PyBot.SendTextMessage("<b>MA NOOO!!!</b>")
 if len(args)<2:
     sys.exit()

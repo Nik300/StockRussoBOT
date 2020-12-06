988e823fda930af9e86e691e4aa8b7428f58c906
w13rsm0 <56513487+w13rsm0@users.noreply.github.com>
2020-12-06 22:05:12 +0100

    Add files via upload

---
+++ Gerry e Davide1.py
@@ -0,0 +1,10 @@
+from BotOS_API import *
+if args is None:
+    PyBot.SendTextMessage("MUSO MARSO NON TI DIMENTICARE CHE QUESTO COMANDO PRENDE DEGLI ARGOMENTI!!!!!!!")
+if args is not None:
+    if args[0]=="Davide":
+        PyBot.SendTextMessage("MA SIAMO AL MILITARE QUI!!!!!!")
+    if args[1]=="Gerry":
+        PyBot.SendTextMessage("<b>MA NOOO!!!</b>")
+if len(args)<2:
+    sys.exit()

e1c7547470cb7804a3a1378125f36a67d33c6714
w13rsm0 <56513487+w13rsm0@users.noreply.github.com>
2020-12-07 19:50:34 +0100

    Add files via upload

--- Emozione.py
+++ Emozione.py
@@ -3,9 +3,9 @@ import sys
 if len(args)==0:
     PyBot.SendTextMessage("ATTENSIUN POPULASIUN, QUESTO COMANDO PRENDE DEGLI ARGOMENTI,IN QUESTA MANIERA NON FA NIENTE, ATTENSIUN!")
 if len(args)>0:
-    if args[0]=="faccina felice":
+    if args[0]=="felice":
         Utilities.ShortnameToEmoji(":(::")
-    elif args[0]=="faccina triste":
+    elif args[0]=="triste":
         Utilities.ShortnameToEmoji("::):")
 if len(args)<1:
     sys.exit()

055083c471f93d4a347a489326ccb2cdc96dd2aa
w13rsm0 <56513487+w13rsm0@users.noreply.github.com>
2020-12-08 17:57:34 +0100

    Add files via upload

--- Emozione.py
+++ Emozione.py
@@ -1,6 +1,7 @@
 from BotOS_API import *
 import sys
 import sys
+import sys
 if len(args)==0:
     PyBot.SendTextMessage("ATTENSIUN POPULASIUN, QUESTO COMANDO PRENDE DEGLI ARGOMENTI,IN QUESTA MANIERA NON FA NIENTE, ATTENSIUN!")
 if len(args)>0:
@@ -10,6 +11,9 @@ if len(args)>0:
             sys.exit()
         PyBot.SendTextMessage(Utilities.ShortnameToEmoji(":smile:"))
     elif args[0]=="triste":
+        if len(args)>1 and args[1]=="pianto":
+            PyBot.SendTextMessage(Utilities.ShortnameToEmoji(":sob:"))
+            sys.exit()
         PyBot.SendTextMessage(Utilities.ShortnameToEmoji(":cry:"))
     elif args[0]=="arrabbiato":
         PyBot.SendTextMessage(Utilities.ShortnameToEmoji(":rage:"))

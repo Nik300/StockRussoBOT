4d3068b642a44a4848f002176d4c4f7ac609b226
F4lc0131 <nicolaflavio22@gmail.com>
2020-12-11 10:43:28 +0100

    Add files via upload

---
+++ clips.js
@@ -0,0 +1,2 @@
+JSBot.SendTextMessage("<b>Entra</b> nel canale delle <b>clip di Gerry</b>(<i>By il nostro fantastico @davidepuato</i>!)\n<i>https://t.me/gerryscotticlips</i>");
+JSBot.SendTextMessage("Questo <i>comando</i> è stato <b>ideato e suggerito</b> da <b>@francodale12 e @errejkappa18</b>!");

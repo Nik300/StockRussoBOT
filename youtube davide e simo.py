1f4aefd3a5dd7870a1c8af6f95578c44dd73c712
w13rsm0 <56513487+w13rsm0@users.noreply.github.com>
2020-12-11 22:27:10 +0100

    Add files via upload

--- youtube davide e simo.py
+++ youtube davide e simo.py
@@ -4,8 +4,8 @@ if len(args)==0:
     PyBot.SendTextMessage("che canale vuoi che ti linki?")
 if len(args)>0:
     if args[0]=="davide":
-        PyBot.SendTextMessage("https://www.youtube.com/results?search_query=stockdroid")
+        PyBot.SendTextMessage("THE CANAL DI DAVIDE:https://www.youtube.com/user/stockdroid")
     elif args[0]=="simone":
-        PyBot.SendTextMessage("https://www.youtube.com/results?search_query=simone+russo")
+        PyBot.SendTextMessage("CANALE DI SIMONEhttps://www.youtube.com/user/MondoMOBILE00")
 if len(args)<1:
     sys.exit()

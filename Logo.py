a49c6b8f4760e8d7cf0c25016ab2a89df1303119
w13rsm0 <56513487+w13rsm0@users.noreply.github.com>
2020-12-15 10:56:02 +0100

    Add files via upload

---
+++ Logo.py
@@ -0,0 +1,9 @@
+from BotOS_API import *
+import sys
+if len(args)== 0:
+    PyBot.SendTextMessage("MUSO MARSO CHE LOGO VUOI?")
+if len(args)>0:
+    if args[0].lower()=="BotOS":
+        PyBot.SendImageMessage("https://raw.githubusercontent.com/Nik300/StockRussoBOT/master/BotOS.jpg")
+if len(args)<1:
+    sys.exit()

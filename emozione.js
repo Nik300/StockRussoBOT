bb4b502f58a01c1c406e7af63c9a1400638bbed8
w13rsm0 <56513487+w13rsm0@users.noreply.github.com>
2020-12-23 14:14:38 +0100

    Add files via upload

--- emozione.js
+++ emozione.js
@@ -1,16 +1,18 @@
 function sistema(){
+    if(args > 0 && args[1] == "risata"){
+        JSBot.SendTextMessage(Utilities.ShortnameToEmoji(":rofl:"));
+}
     if(args[0] == "felice"){
         JSBot.SendTextMessage(Utilities.ShortnameToEmoji(":smile:"));
-        if(args > 0 && args[1] == "risata"){
-            JSBot.SendTextMessage(Utilities.ShortnameToEmoji(":rofl:"));
-    }
+        
     
     }
-    else if(args[0] == "triste"){
+    else if(args > 0 && args[1] == "pianto"){
+        JSBot.SendTextMessage(Utilities.ShortnameToEmoji(":sob:"));
+    }
+    if(args[0] == "triste"){
         JSBot.SendTextMessage(Utilities.ShortnameToEmoji(":cry:"));
-        if(args > 0 && args[1] == "pianto"){
-             JSBot.SendTextMessage(Utilities.ShortnameToEmoji(":sob:"));
-         }
+       
     }
 
 }

b062adbcc3bcbc045a00e08d59ca5c8fb9cfb59d
mattiafenzi <70102761+mattiafenzi@users.noreply.github.com>
2020-12-24 11:00:49 +0100

    Add files via upload

---
+++ annalisamode.js
@@ -0,0 +1,8 @@
+if(args==null)
+{
+    JSBot.SendTextMessage("Adesso sei in modalità Annalisa, d'ora in poi sarai bipolare");
+}
+else
+{
+    JSBot.SendTextMessage("Hai forzato @"+args[0]+" in modalità Annalisa, fai attenzione da adesso @"+args[0]+" è bipolare");
+}

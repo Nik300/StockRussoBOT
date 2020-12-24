3804cccbba878a03f24033897985926395d0adb6
mattiafenzi <70102761+mattiafenzi@users.noreply.github.com>
2020-12-24 11:39:02 +0100

    Add files via upload

---
+++ seimarso.js
@@ -0,0 +1,8 @@
+if(args==null)
+{
+    JSBot.SendVideoMessage("https://raw.githubusercontent.com/Nik300/StockRussoBOT/master/SEI MARSO.mp4");
+}
+else
+{
+    JSBot.SendVideoMessage("https://raw.githubusercontent.com/Nik300/StockRussoBOT/master/SEI MARSO.mp4", args[0]+" se lo dice Gerry vuol dire che lo sei proprio");
+}

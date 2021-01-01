f082837b398b834363c581ff0c09b984feed625e
mattiafenzi <70102761+mattiafenzi@users.noreply.github.com>
2021-01-01 14:47:02 +0100

    Add files via upload

---
+++ buonasera.js
@@ -0,0 +1,8 @@
+if(args==null)
+{
+    JSBot.ReplyVideoMessage("https://raw.githubusercontent.com/Nik300/StockRussoBOT/master/buonasera (1).mp4");
+}
+else
+{
+    JSBot.ReplyVideoMessage("https://raw.githubusercontent.com/Nik300/StockRussoBOT/master/buonasera (1).mp4", args[0]);
+}

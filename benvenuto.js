264088aedbfc74a41572a9a73acba8205042741e
mattiafenzi <70102761+mattiafenzi@users.noreply.github.com>
2021-01-01 16:23:11 +0100

    Update benvenuto.js

--- benvenuto.js
+++ benvenuto.js
@@ -4,7 +4,7 @@ if(args==null)
 }
 else
 {
-    let utente=JSBot.MessageArgs.Message.From.Username;
+    let utente=JSBot.From.Username;
     if(args[0]=="maschio")
     {
         JSBot.ReplyVideoMessage("https://raw.githubusercontent.com/Nik300/StockRussoBOT/master/video_2021-01-01_14-48-50.mp4", utente+" ti da il benvenuto") 

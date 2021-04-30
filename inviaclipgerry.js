368e0f6ebd741dd51b8742e560c8a8bcb907edee
w13rsm0 <56513487+w13rsm0@users.noreply.github.com>
2021-04-30 13:59:16 +0200

    Add files via upload

--- inviaclipgerry.js
+++ inviaclipgerry.js
@@ -6,5 +6,14 @@ function buonasuora(){
     if(args[0] == "auguri"){
         JSBot.SendVideoMessage("https://raw.githubusercontent.com/Nik300/StockRussoBOT/master/AUGURI!.mp4");
     }
+    else if(args[0] == "allora"){
+        JSBot.SendVideoMessage("https://raw.githubusercontent.com/Nik300/StockRussoBOT/master/ALLORA.mp4");
+    }
+    else if(args[0] == "info"){
+        JSBot.SendTextMessage("Comando che invia clip di gerry, come argomento metti cosa è detto nella clip abbiamo per ora: auguri e allora");
+    }
+    else if(args[0] == "ver"){
+        JSBot.SendTextMessage("0.0.1")
+    } 
 } 
-buonasuora(); 
+buonasuora();

2d65eb73e1a603bce70478c6a919fb96f3c3857a
w13rsm0 <56513487+w13rsm0@users.noreply.github.com>
2021-04-30 13:42:05 +0200

    Add files via upload

--- inviaclipgerry.js
+++ inviaclipgerry.js
@@ -3,7 +3,7 @@ function buonasuora(){
         JSBot.SendTextMessage("MA CHE CLIP VUOI?");
         return;
     }
-    if(args[0] == auguri){
+    if(args[0] == "auguri"){
         JSBot.SendVideoMessage("https://raw.githubusercontent.com/Nik300/StockRussoBOT/master/ALLORA.mp4");
     }
 } 
